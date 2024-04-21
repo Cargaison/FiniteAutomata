3ec529d081c2e035b34b56d73be2ef20f5a16961
Cargaison <maxence.leclercq297@gmail.com>
2024-04-21 23:55:32 +0200

    corrected versions

--- engine.py
+++ engine.py
@@ -1,15 +1,12 @@
 import os
-
-# Directory where the files will be created
+#just a little code to create all the files
 folder_path = os.getcwd()
 
-# Loop to create files from automata1.txt to automata44.txt
 for i in range(20, 30):
     file_name = f"{i}.txt"
     file_path = os.path.join(folder_path, file_name)
 
-    # Create the file and close it
     with open(file_path, 'w') as file:
-        pass  # The 'pass' statement is used here to indicate an empty block
+        pass
 
 print(f"44 files have been created in {folder_path}")
